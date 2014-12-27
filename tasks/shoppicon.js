98f83a9b930947e61ec9fb7129f3a842e8c7542e
Ryan Frederick <ryan@ryanfrederick.com>
2014-12-26 20:10:47 -0800

    Try to make Grunt load the correct tasks when shoppicon is loaded as a module.

--- tasks/shoppicon.js
+++ tasks/shoppicon.js
@@ -6,9 +6,20 @@
 
 'use strict';
 
+var path = require('path');
+
 module.exports = function(grunt) {
+    // Change from the Gruntfile’s dir to the local shoppicon dir so that
+    // loadNpmTasks finds the dependencies.
+    var cwd = process.cwd();
+    process.chdir(path.join(__dirname, '../'));
+
+    // Load tasks from shoppicon’s dependencies.
     grunt.loadNpmTasks('grunt-svgstore');
     grunt.loadNpmTasks('grunt-svgmin');
+
+    // Restore the working directory.
+    process.chdir(cwd);
 
     grunt.registerMultiTask('shoppicon', 'TODO Description', function() {
         grunt.config.merge({
